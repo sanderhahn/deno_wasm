6310123fb8e43b22250945c2fd56238e32d6542f
Sander Hahn <sanderhahn@gmail.com>
2020-11-04 07:56:54 +0100

    web-sys log to console

---
+++ examples/wasmbindgen/websys/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+use js_sys::JsString;
+
+#[wasm_bindgen]
+pub fn hello_console() -> () {
+    let hello: JsString = "Hello world".into();
+    let args = js_sys::Array::new();
+    args.push(&hello);
+    web_sys::console::log(&args);
+}
